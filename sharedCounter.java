b8b3a95f456de0e8faaf08b3d5394ae2960f2a26
CSC-383 <78540948+CSC-383@users.noreply.github.com>
2021-04-25 03:16:41 +0600

    Update sharedCounter.java

--- sharedCounter.java
+++ sharedCounter.java
@@ -1,37 +1,69 @@
 // question no 12
+
 package labreport;
 
-class Counter {
+class sharedCounter implements Runnable {
 
-    private int val;
+    int count = 0;
 
-    public Counter() {
-        val = 0;
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            count = count + 1;
+
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " : " + count);
+        
+
     }
 
-    public void increment() {
-        val = val + 1;
-    }
+}
 
-    public int getVal() {
-        return val;
+class count {
+
+    public static void main(String args[]) {
+        Thread t1 = new Thread(new sharedCounter());
+        Thread t2 = new Thread(new sharedCounter());
+        Thread t3 = new Thread(new sharedCounter());
+        Thread t4 = new Thread(new sharedCounter());
+        Thread t5 = new Thread(new sharedCounter());
+        Thread t6 = new Thread(new sharedCounter());
+        Thread t7 = new Thread(new sharedCounter());
+        Thread t8 = new Thread(new sharedCounter());
+        Thread t9 = new Thread(new sharedCounter());
+        Thread t10 = new Thread(new sharedCounter());
+
+        t1.setName("Thread-1");
+        t2.setName("Thread-2");
+        t3.setName("Thread-3");
+        t4.setName("Thread-4");
+        t5.setName("Thread-5");
+        t6.setName("Thread-6");
+        t7.setName("Thread-7");
+        t8.setName("Thread-8");
+        t9.setName("Thread-9");
+        t10.setName("Thread-10");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
+        t8.start();
+        t9.start();
+        t10.start();
+
     }
 }
 
-class IncThread extends Thread {
-
-    int numOfThread = 10;
-    Counter myCounter;
-
-    public IncThread(Counter c) {
-        myCounter = c;
-    }
-
-    public void run() {
-        for (int i = 1; i <= numOfThread; i++) {
-            myCounter.increment();
-        }
-    }
 
 }
 
